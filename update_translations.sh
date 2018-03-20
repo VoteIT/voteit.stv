ff25924b596542136868a44879fbef1eda25a73f
Robin Harms Oredsson <robin@betahaus.net>
2018-03-20 17:25:42 +0100

    Translation fix

--- update_translations.sh
+++
@@ -1,9 +0,0 @@
- #!/bin/bash
- #You need lingua and gettext installed to run this
- 
- echo "Updating voteit.stv.pot"
- pot-create -d voteit.stv -o voteit/stv/locale/voteit.stv.pot .
- echo "Merging Swedish localisation"
- msgmerge --update voteit/stv/locale/sv/LC_MESSAGES/voteit.stv.po voteit/stv/locale/voteit.stv.pot
- echo "Updated locale files"
- 
